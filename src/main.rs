d3062d97420ff17f2cedc1554b5d3b5a4a8810cb
0kalekale <kalekale.anon@gmail.com>
2021-02-04 12:46:18 +0545

    added vga module

--- src/main.rs
+++ src/main.rs
@@ -3,24 +3,20 @@
 
 use core::panic::PanicInfo;
 
-static HELLO: &[u8] = b"test";
+mod vga;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    println!("Loading kernel...");
+    println!("...");
+    println!("Loaded Cunny kernel.");
 
     loop {}
 }
 
 /// This function is called on panic.
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
